4d943f0afbe744631295d88df7d2587f8bf4f5be
viniciussm30 <vinicius.marinelli@etec.sp.gov.br>
2020-04-21 00:49:24 -0300

    atualização nos codigos index cadastroCliente Menu Inicializações

---
+++ root/js/inicializacoes.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Menu mobile 1
+    $('#menuMobile1').sidenav();
+    // Select
+    $('select').formSelect();
+});
---
+++ root/js/menu.js
@@ -0,0 +1,4 @@
+$('.home').click(function() {
+    $('#conteudo').empty();
+    $('#conteudo').load('cliente/visao/cadastroCliente.html');
+});
