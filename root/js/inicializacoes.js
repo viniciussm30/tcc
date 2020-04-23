32502312a699c87ba4ca2c2d357ef5fb5fbd0820
viniciussm30 <vinicius.marinelli@etec.sp.gov.br>
2020-04-23 00:01:14 -0300

    testeLayoutPagina

--- root/js/inicializacoes.js
+++ root/js/inicializacoes.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
     // Slider
-    $('.slider').slider();
+    $('.slider').slider({
+
+        indicators: false
+    });
     // Menu mobile 1
     $('#menuMobile1').sidenav();
     // Select
     $('select').formSelect();
-    // Carousel
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true,
-        duration: 100
+
+    $('.next').click(function() {
+        $('.carousel.carousel-slider').carousel('next')
     });
+
+
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 0
+    });
+
+
 
 });
